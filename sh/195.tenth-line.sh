7b7232808c2f182894f679448bbf25b86cf48de2
maguro <maguro.devlife@gmail.com>
2022-01-19 13:59:49 +0900

    Add sh-solutions

---
+++ sh/195.tenth-line.sh
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=195 lang=bash
+#
+# [195] Tenth Line
+#
+
+# @lc code=start
+# Read from the file file.txt and output the tenth line to stdout.
+head -n 10 file.txt | tail -n +10
+
+# @lc code=end
+
