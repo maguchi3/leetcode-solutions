a9b48c1dfb5963c6eee273bae6f2343bbc555633
maguro <maguro.devlife@gmail.com>
2022-01-27 14:40:21 +0900

    chore: add utils for ListNode-[]int conversion

--- go/structures/list-node.go
+++ go/structures/list-node.go
@@ -4,3 +4,29 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func IntsToList(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	dummy := &ListNode{}
+	head := dummy
+
+	for _, v := range nums {
+		head.Next = &ListNode{Val: v}
+		head = head.Next
+	}
+
+	return dummy.Next
+}
+
+func ListToInts(head *ListNode) []int {
+	res := []int{}
+
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
