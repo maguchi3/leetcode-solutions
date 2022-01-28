6130195435187d0743e1d1676019fb6774a6122a
maguro <maguro.devlife@gmail.com>
2022-01-28 14:54:06 +0900

    chore: define struct and util of TreeNode

---
+++ go/structures/tree-node.go
@@ -0,0 +1,36 @@
+package structures
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+const NULL = -1 << 63 // using express null node val
+
+func IntsToTree(ints []int) *TreeNode {
+	n := len(ints)
+	root := &TreeNode{Val: ints[0]}
+	q := make([]*TreeNode, 1, n)
+	q[0] = root
+
+	i := 1
+
+	for i < n {
+		node := q[0]
+		q = q[1:]
+
+		if ints[i] != NULL {
+			node.Left = &TreeNode{Val: ints[i]}
+			q = append(q, node.Left)
+		}
+		i++
+		if i < n && ints[i] != NULL {
+			node.Right = &TreeNode{Val: ints[i]}
+			q = append(q, node.Right)
+		}
+		i++
+	}
+
+	return root
+}
